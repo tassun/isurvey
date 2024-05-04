27008a8531faaf72e1baad52cf76127b3137ac6a
tassun <32536248+tassun@users.noreply.github.com>
2024-05-04 16:39:08 +0700

    Update SurveyDHandler.ts

--- src/handlers/SurveyDHandler.ts
+++ src/handlers/SurveyDHandler.ts
@@ -122,25 +122,25 @@ export class SurveyDHandler extends SurveyOperateHandler {
 
     public override async processInsert(context: KnContextInfo, db: KnDBConnector) : Promise<KnRecordSet> {
         let rs = await super.processInsert(context, db);        
-        let sql = new KnSQL();
-        sql.append("UPDATE survey_dx set survey_state = 'CONFIRM' ");
-        sql.append("WHERE master_id = ?master_id ");
-        sql.set("master_id", rs.rows.survey_id);
-        this.logger.info(this.constructor.name+".processInsert:",sql);
-        await sql.executeUpdate(db,context);
+        await this.processUpdateState(context, db, rs.rows.survey_id);
         return Promise.resolve(rs);
     }
 
     public override async processUpdate(context: KnContextInfo, db: KnDBConnector) : Promise<KnRecordSet> {
         let survey_id = context.params.survey_id;
         let rs = await super.processUpdate(context, db);
+        await this.processUpdateState(context, db, survey_id);
+        return Promise.resolve(rs);
+    }
+
+    public async processUpdateState(context: KnContextInfo, db: KnDBConnector, survey_id: string) : Promise<KnRecordSet> {
         let sql = new KnSQL();
         sql.append("UPDATE survey_dx set survey_state = 'CONFIRM' ");
         sql.append("WHERE master_id = ?master_id ");
         sql.set("master_id", survey_id);
-        this.logger.info(this.constructor.name+".processUpdate:",sql);
-        await sql.executeUpdate(db,context);
-        return Promise.resolve(rs);
+        this.logger.info(this.constructor.name+".processUpdateState:",sql);
+        let rs = await sql.executeUpdate(db,context);
+        return Promise.resolve(this.createRecordSet(rs));
     }
-
+    
 }
