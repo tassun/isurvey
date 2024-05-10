6415d23083c32c3d20c1439164fdbd7c192c6d4f
tassun <32536248+tassun@users.noreply.github.com>
2024-05-10 12:13:17 +0700

    Update SurveyB7Handler.ts

--- src/handlers/SurveyB7Handler.ts
+++ src/handlers/SurveyB7Handler.ts
@@ -1,8 +1,5 @@
-import { KnDBConnector, KnRecordSet, KnSQL } from 'will-sql';
-import { KnModel, KnContextInfo, KnDataTable, KnValidateInfo } from '../models/AssureAlias';
+import { KnModel } from '../models/AssureAlias';
 import { SurveyMasterHandler } from './SurveyMasterHandler';
-import { VerifyError } from '../models/VerifyError';
-import { HTTP } from '../api/HTTP';
 
 export class SurveyB7Handler extends SurveyMasterHandler {
     public readonly form_id : string = "SURVEY_B7";
@@ -83,40 +80,4 @@ export class SurveyB7Handler extends SurveyMasterHandler {
         }
     };
 
-    /*
-    protected async validateRequireFieldsList(context: KnContextInfo, throwError: boolean = false) : Promise<KnValidateInfo> {
-        let vi = this.validateParameters(context.params,"master_id");
-        if(!vi.valid && throwError) {
-            return Promise.reject(new VerifyError("Parameter not found ("+vi.info+")",HTTP.NOT_ACCEPTABLE,-16061));
-        }
-        return Promise.resolve(vi);
-    }
-
-    public override async processList(context: KnContextInfo, db: KnDBConnector) : Promise<KnRecordSet> {
-        await this.validateRequireFieldsList(context,true);
-        let master_id = context.params.master_id;
-        let sql = new KnSQL();
-        sql.append("select * from ").append(this.model.name).append(" where master_id = ?master_id ");
-        sql.set("master_id",master_id);
-        this.logger.info(this.constructor.name+".processList:",sql);
-        let rs = await sql.executeQuery(db,context);
-        return Promise.resolve(this.createRecordSet(rs));
-    }
-
-    public override async getDataListing(context: KnContextInfo, db: KnDBConnector, rs: KnRecordSet) : Promise<KnDataTable> {
-        let dt = await super.getDataListing(context,db,rs);
-        let survey_id = context.params.survey_id;
-        let master_id = context.params.master_id;
-        dt.dataset.survey_id = survey_id;
-        dt.dataset.master_id = master_id;
-        return Promise.resolve(dt);    
-    }
-
-    public override async getDataAdd(context: KnContextInfo) : Promise<KnDataTable> {
-        let dt = await super.getDataAdd(context);
-        dt.dataset.survey_id = context.params.survey_id;
-        dt.dataset.master_id = context.params.master_id;
-        return Promise.resolve(dt);
-    }
-    */
 }
