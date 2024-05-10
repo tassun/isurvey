79ea51e970315b1e3fb0d07d151bf60d94edda33
tassun <32536248+tassun@users.noreply.github.com>
2024-05-10 17:22:38 +0700

    support cluster

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,23 @@
 import { KnExpress } from "./runner/KnExpress";
 import { RouteManager } from './manager/RouteManager';
+import { APP_INSTANCES } from './utils/EnvironmentVariable';
 
-let app = KnExpress.createApplication();
-console.log("working directory",__dirname);
-new RouteManager(__dirname).route(app);
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+console.log("number of CPU",numCPUs);
+let numOfInstances = numCPUs;
+if(APP_INSTANCES != "CPU") {
+    numOfInstances = parseInt(APP_INSTANCES);
+}
+if (numOfInstances > 1 && cluster.isMaster) {
+    for (let i = 0; i < numOfInstances; i++) {
+        cluster.fork();
+    }        
+    cluster.on('exit', (worker:any, code: any, signal: any) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    let app = KnExpress.createApplication();
+    console.log("working directory",__dirname);
+    new RouteManager(__dirname).route(app);
+}
