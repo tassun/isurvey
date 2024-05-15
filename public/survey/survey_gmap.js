1a434def7a8442e6bb34a9fbb286168adb2cb28c
tassun <32536248+tassun@users.noreply.github.com>
2024-05-15 09:12:46 +0700

    Update survey_gmap.js

--- public/survey/survey_gmap.js
+++ public/survey/survey_gmap.js
@@ -7,7 +7,6 @@ function setupGmap(){
             return;
         }
 		initMap();
-		//$('#gmapbutton').hide();
 		$('#gmap-layer').show();
 	});
 	$('#choosemapbutton').click(function(){
@@ -19,44 +18,56 @@ function setupGmap(){
 	});	
 }
 function choosePosition() {
-    if(latLng.lat && latLng.lat!="" && latLng.lng && latLng.lng!="") {
+    if(latLng.lat && latLng.lng) {
         $("#A3").val((latLng.lat || '') + "," + (latLng.lng || ''));
     }
 	$('#gmap-layer').hide();
 	$('#gmapbutton').show();
 }
+var googlemap = null;
+var googlemarker = null;
+function createMap() {
+	let len = $("#gmap").children().length;
+	if(!googlemap || len==0) {
+		googlemarker = null;
+		var mapOptions = {
+			center: {lat: 13.847077, lng: 100.606973},
+			zoom: 15,
+		}
+		googlemap = new google.maps.Map(document.getElementById("gmap"),mapOptions);	
+		googlemap.addListener('click', function(position) {
+			console.log(position);
+			if(googlemarker) {
+				googlemarker.setMap(null);
+			}
+			latLng.lat = position.latLng.lat();
+			latLng.lng =  position.latLng.lng();
+			googlemarker = new google.maps.Marker({
+				position: position.latLng,
+				map: googlemap,
+				title: 'Marker'
+			});
+		});
+	}
+	return googlemap;
+}
 function initMap(){	
+	createMap();
     let locate = $("#A3").val().split(",");
 	let lat = locate.length>0?locate[0]:'';
 	let lng = locate.length>1?locate[1]:'';
-	var marker;
-	var mapOptions = {
-        center: {lat: 13.847077, lng: 100.606973},
-        zoom: 15,
-    }
-	var map = new google.maps.Map(document.getElementById("gmap"),mapOptions);	
-	if(lat && lat!="" && lng && lng!="") {
+	if(lat && $.trim(lat)!="" && lng && $.trim(lng)!="") {
 		lat = parseFloat(lat);
 		lng = parseFloat(lng);
 		console.log(lat, lng);
-		map.setCenter(new google.maps.LatLng(lat, lng));
-		marker = new google.maps.Marker({
+		googlemap.setCenter(new google.maps.LatLng(lat, lng));
+		if(googlemarker) {
+			googlemarker.setMap(null);
+		}
+		googlemarker = new google.maps.Marker({
 			   position: new google.maps.LatLng(lat, lng),
-			   map: map,
+			   map: googlemap,
 			   title: "Marker"
         });		
 	}
-	map.addListener('click', function(position) {
-		console.log(position);
-		if(marker){
-			marker.setMap(null);
-		}
-		latLng.lat = position.latLng.lat();
-		latLng.lng =  position.latLng.lng();
-		marker = new google.maps.Marker({
-			   position: position.latLng,
-			   map: map,
-			   title: 'Marker'
-			});
-      });
 }
